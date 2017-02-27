9c75f1e848790a47860784ad16e5d42d87e4736a
Johnny Wu <KarasAya@gmail.com>
2017-02-27 11:20:13 +0800

    simple refine

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const platform = require('electron-platform');
+const pkgJson = require('./package.json');
 
 let windowPlus;
+let name = `__electron_window_plus__`;
+let msg = `Failed to require ${pkgJson.name}@${pkgJson.version}:
+  A different version of ${pkgJson.name} already running in the process, we will redirect to it.
+  Please make sure your dependencies use the same version of ${pkgJson.name}.`;
 
 if ( platform.isMainProcess ) {
-  windowPlus = require('./lib/main');
+  if (global[name]) {
+    console.warn(msg);
+    windowPlus = global[name];
+  } else {
+    windowPlus = global[name] = require('./lib/main');
+  }
 } else {
   // windowPlus = require('./lib/renderer/index');
 }
